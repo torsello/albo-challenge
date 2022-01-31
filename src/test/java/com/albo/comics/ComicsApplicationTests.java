3724866a4b1b15e9ba4ee264a6c8d5f76019aa1f
torsello <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>
2022-01-31 16:42:24 -0300

    Base project

---
+++ src/test/java/com/albo/comics/ComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albo.comics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
